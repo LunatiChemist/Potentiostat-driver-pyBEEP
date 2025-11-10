912abd44ade971bc450eb022768704cb6ed5d815
LunatiChemist <166641604+LunatiChemist@users.noreply.github.com>
2025-11-10 15:03:09 -0800

    made cdl calculation better

--- examples/example_Cdl.py
+++ examples/example_Cdl.py
@@ -3,6 +3,7 @@ import logging
 from pyBEEP import (
     setup_logging,
     connect_to_potentiostat,
+    #plot_cdl_points
 )
 
 setup_logging(level=logging.INFO)
@@ -16,12 +17,17 @@ os.makedirs(folder, exist_ok=True)
 cdl_params = {
     "vertex_a": -2.0,
     "vertex_b": 2.0,
-    "scan_rates": [1.0,0.9,0.8,0.7,0.6,0.5],
+    "scan_rates": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],  # [V/s]
     "cycles_per_rate": 2,
     "rest_time": 5.0,
-    "start": None, # optional
-    "end": None # optional
+    "start": None,  # optional
+    "end": None,  # optional
 }
+
+cdl_filename = "test_CDL.csv"
+
 controller.apply_measurement(
-    mode="CDL", params=cdl_params, tia_gain=0, filename="test_Cdl.csv", folder=folder
+    mode="CDL", params=cdl_params, tia_gain=0, filename=cdl_filename, folder=folder
 )
+
+csv_path = os.path.join(folder, cdl_filename)
